d542d5c090ce0732e864b30f87aa2ccbbf2e878b
Vignesh2k0508 <bvignesh220@gmail.com>
2023-12-10 19:48:02 +0530

    Changes have been made

--- src/Components/Home_page/HomePageContent.jsx
+++ src/Components/Home_page/HomePageContent.jsx
@@ -4,8 +4,7 @@ const HomePageContent=()=>{
     return(
         <div className="home-content">
             <h1 id="Home-title">CLEAN AND FLEXIBLE TEMPLATE</h1>
-            <p id="Home-description">Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
-            Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. </p>
+            <p id="Home-description">Wake up to reality! Nothing ever goes as planned in this world. The longer you live, the more you realize that in this reality only pain, suffering and futility exist. </p>
             <div id="home-btns">
                 <button id="btn-left">Download now</button>
                 <button id="btn-right">view features</button>
